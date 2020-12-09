2db2c18a9a3894fe7081aafb0bba82bd0ce08dad
Aron Gabriel <aronogayon@gmail.com>
2020-12-09 22:37:34 +0800

    Created the tilemap data to png

--- 2DTopdownShooter/Scripts/Player.gd
+++ 2DTopdownShooter/Scripts/Player.gd
@@ -1,24 +1,35 @@
 extends KinematicBody2D
 
 export (float) var speed := 300
-export (float) var dash_multiplier := 2
+export (float) var dash_multiplier := 2.0
 export (float) var dash_time := 0.5 # Still don't know what to name it...
 
 var _velocity := Vector2.ZERO
 var _dash_timer := 0.0
 var _dashing := false
 
+var _last_direction = Vector2.ZERO
+
+func _process(delta: float) -> void:
+	look_at(get_global_mouse_position())
+
 func _physics_process(delta: float) -> void:
 	var direction := Vector2.ZERO
 	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	
+	if direction.length_squared() > 0:
+		_last_direction = direction
+	
 	if Input.is_action_just_pressed("dash"):
 		_dashing = true
+	# I will probably add this... I still don't know though
+#	var mouse_position = get_viewport().get_mouse_position()
+#	var distance: Vector2 = (mouse_position - global_position).normalized()
 	
 	if _dashing:
 		_dash_timer += delta
-		_velocity = direction.normalized() * speed * dash_multiplier
+		_velocity = _last_direction * speed * dash_multiplier
 		if _dash_timer > dash_time:
 			_dash_timer = 0.0
 			_dashing = false
